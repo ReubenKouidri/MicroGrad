58aa6d7ba730f0db2b1be31420bdb2eae7605484
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-10-15 16:53:06 +0100

    added test for __add__

--- tests/test_value.py
+++ tests/test_value.py
@@ -1,9 +1,22 @@
 import unittest
+from src.value import Value
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.v1 = Value(1.0)
+        self.v2 = Value(2.0)
+        self.v3 = self.v1 + self.v2
+        self.v3.backward()
+
+    def test_addition(self):
+        self.assertTrue(isinstance(self.v3, Value))
+        self.assertEqual(self.v3.data, self.v1.data + self.v2.data)
+        self.assertEqual(self.v1.grad, 1.0)
+        self.assertEqual(self.v2.grad, 1.0)
+        self.assertEqual(self.v3.grad, 1.0)
+        self.assertEqual(self.v3.prev, {self.v1, self.v2})
+        self.assertEqual(self.v3.op, "+")
 
 
 if __name__ == '__main__':
