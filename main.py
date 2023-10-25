bec2da7dca81e22a2ce1cfb86eaf2b0a9d505380
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-10-25 21:16:01 +0100

    investigates torch vs handbuilt model

--- main.py
+++ main.py
@@ -1,47 +1,64 @@
 from src.neuron import NeuralNetwork
 from src.profiler import profile
-import numpy as np
+from src.torch_model import TorchMLP
 import random
+import torch
+import torch.nn as nn
+from torch.optim import Adam
 
-EPOCHS = 500
-LR = 0.1
+
+random.seed(982375)
+torch.random.manual_seed(982375)
+EPOCHS = 50
+LR = 0.01
+TORCH = True
 
 
 @profile
-def train(nn, X, Y, EPOCHS, LR):
-    y_final = []
+def train(model, X, Y, EPOCHS, LR):
+    if TORCH:
+        optimizer = Adam(model.parameters(), LR)
+        criterion = nn.MSELoss()
+        model.train()
+        X = torch.as_tensor(X, dtype=torch.float32)
+        Y = torch.as_tensor(Y, dtype=torch.float32).unsqueeze(1)
+
     for epoch in range(EPOCHS):
-        ypred = [nn(x) for x in X]
-        loss = sum((yp - yt) ** 2 for yp, yt in zip(ypred, Y))
+        if TORCH:
+            optimizer.zero_grad()
+            preds = model(X)
+            loss = criterion(preds, Y)
+            loss.backward()
+            optimizer.step()
 
-        for p in nn.parameters():
-            p.grad = 0
+            if epoch == EPOCHS - 1:
+                y_final = preds
+        else:
+            ypred = [model(x) for x in X]
+            loss = sum((yp - yt) ** 2 for yp, yt in zip(ypred, Y))
+            for p in model.parameters():
+                p.grad = 0
 
-        loss.backward()
+            loss.backward()
+            for p in model.parameters():
+                p.data -= LR * p.grad
 
-        for p in nn.parameters():
-            p.data -= LR * p.grad
-
-        if epoch == EPOCHS - 1:
-            y_final = ypred
+            if epoch == EPOCHS - 1:
+                y_final = ypred
 
     print(f"preds: {y_final}")
 
 
 if __name__ == '__main__':
-
     nouts = [4, 4, 1]
-    nin = 100
+    nin = 3
     num_classes = 4
-    nn = NeuralNetwork(nin, nouts)  # [3, [4, 4, 1]]
 
-    inputs = [[random.randint(-3, 3) for _ in range(nin)] for c in range(num_classes)]
+    if TORCH:
+        model = TorchMLP(nin, nouts)
+    else:
+        model = NeuralNetwork(nin, nouts)
 
-    # X = [[2.0, 3.0, -1.0],
-    #      [3.0, -1.0, 0.5],
-    #      [0.5, 1.0, 1.0],
-    #      [1.0, 1.0, -1.0]]
-
+    X = [[random.randint(-3, 3) for _ in range(nin)] for c in range(num_classes)]
     Y = [1.0, -1.0, -1.0, 1.0]
-
-    train(nn, inputs, Y, EPOCHS, LR)
+    train(model, X, Y, EPOCHS, LR)
